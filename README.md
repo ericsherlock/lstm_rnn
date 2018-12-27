13ac8096d93de27c122d5c10cbddf03506463833
ericsherlock <ericsherlock@github.com>
2018-12-26 22:51:54 -0700

    Adding Emotion Classification, Cleaning/Commenting Code

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
-Sentiment Analysis using Long Short Term Memory Recurrent Neural Networks
+Binary Sentiment Analysis and Emotion Classification using Long Short Term Memory Recurrent Neural Networks (LSTM-RNNS)
 
-Using Googles Word2Vec for the word embeddings
+Written with Python and Keras, this code tests binary sentiment analysis with LSTM-RNNS which was extended to classify emotions in text. Please note, this code does not run "out of the box". Due to the size of the datasets and individual machine limitations, in order for the code to run you will need to download the desired datasets and word embeddings. With them, you will need to reshape the matrices and tensors to your specific needs. Resources for the word embeddings and datasets are below. 
+
+Embeddings:
+1) Google's Word2Vec
 	URL: https://code.google.com/archive/p/word2vec/
 
 Datasets:
-
 1) Keras IMDB Dataset 
+	URL: https://keras.io/datasets/
+
 2) Amazon Product Reviews
 	URL: www.kaggle.com/bittlingmayer/amazonreviews
+
+3) Figure Eight's Emotion in Text Dataset:
+	URL: https://www.figure-eight.com/data-for-everyone/
--- sentiment_RNN.py
+++ sentiment_RNN.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 #Import Necessary Packages
+from __future__ import print_function
 from keras.preprocessing import sequence
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
@@ -9,47 +10,70 @@ from keras.preprocessing.text import Tokenizer
 from keras.utils import np_utils
 from keras.layers import LSTM
 from keras.layers import Dropout
+from keras.layers import Flatten
+from keras import optimizers
 from keras.datasets import imdb
 import numpy as np
 from keras.datasets import mnist
 from keras.preprocessing.text import one_hot
 from matplotlib import pyplot
+import matplotlib
 
-#Read In Text File
-#IMDB Dataset Preprocessing
-"""print('Loading data...')
-(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=5000)
-print(len(x_train), 'train sequences')
-print(len(x_test), 'test sequences')
-print('Pad sequences (samples x time)')
-x_train = sequence.pad_sequences(x_train, maxlen=100)
-x_test = sequence.pad_sequences(x_test, maxlen=100)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
-"""
-#Amazon Dataset Preprocessing
-corpora = open("smallText.txt", "r").readlines()
-corpLab = [1]*(len(corpora)/2)
-corpLab.extend([0]*(len(corpora)/2))
+#Read In Text Datasets
+#########################IMDB Dataset Preprocessing#############################
+#Read In The IMDB Dataset and Print Training and Testing Lengths
+#(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=5000)
+#print(len(x_train), 'train sequences')
+#print(len(x_test), 'test sequences')
 
+#Pad The Training and Testing Sequences
+#x_train = sequence.pad_sequences(x_train, maxlen=75)
+#x_test = sequence.pad_sequences(x_test, maxlen=75)
+#print('x_train shape:', x_train.shape)
+#print('x_test shape:', x_test.shape)
+
+#######################Amazon/Emotion in Text Datasets Preprocessing############
+
+#Read In Either The Amazon Dataset or the Emotion in Text Dataset
+#corpora = open("AmazonTextDataset.txt", "r").readlines()
+corpora = open("EmotionInTextDataset.txt", "r").readlines()
+
+#Create Dataset Matrix For Binary/Multiple Classification
+
+#Binary Sentiment Matrix Creation
+#corpLab = [1]*(len(corpora)/2)
+#corpLab.extend([0]*(len(corpora)/2))
+
+#Emotion Classification Matrix 
+corpLab = [0]*(len(corpora)/5)
+corpLab.extend([1]*(len(corpora)/5))
+corpLab.extend([2]*(len(corpora)/5))
+corpLab.extend([3]*(len(corpora)/5))
+corpLab.extend([4]*(len(corpora)/5))
+ 
 #Set Up Tokenizer
-tokenizer = Tokenizer(nb_words=5000)
+tokenizer = Tokenizer(nb_words=10000)
 tokenizer.fit_on_texts(corpora)
 textSeq = tokenizer.texts_to_sequences(corpora)
 wordIndex = tokenizer.word_index
 
 #Pad The Sentences To The Required Length
-data = pad_sequences(textSeq, maxlen=100)
-corpLab = np_utils.to_categorical(np.asarray(corpLab)).reshape(-1)
-print('Shape of Sentence Matrix', data.shape)
-print('Shape of Label Matrix', corpLab.shape)
+data = pad_sequences(textSeq, maxlen=75)
 
-#ch
+#Bi-Sentiment, Reshape Corpora Matrix
+#(Reshape For Binary Sentiment Analysis With Single Dense Node)
+#corpLab = np_utils.to_categorical(np.asarray(corpLab)).reshape(-1)
+#(Reshape For Binary Sentiment Analysis With Two Dense Nodes)
+#corpLab = np_utils.to_categorical(np.asarray(corpLab)).reshape(10814, 2)
+
+#5-Sentiment, Reshape Corpora Matrix
+corpLab = np_utils.to_categorical(np.asarray(corpLab)).reshape(19395, 5)
+
 #Randomly Shuffle Data To Split Into Training And Testing
 index = np.arange(data.shape[0])
 np.random.shuffle(index)
 data = data[index]
-corpLab = corpLab[index]
+corpLab = corpLab[index-5]
 corpSplit = int(.1 * data.shape[0])
 
 #Split The Dataset Into Training And Testing
@@ -57,7 +81,6 @@ x_train = data[:-corpSplit]
 x_test = data[-corpSplit:]
 y_train = corpLab[:-corpSplit]
 y_test = corpLab[-corpSplit:]
-#ch
 
 #Check Shapes Of Training and Testing
 print("SHAPE XTRAIN: ", x_train.shape)
@@ -76,7 +99,7 @@ for line in embedFile:
     embedIndex[word] = wordNumList
     embedDim = len(wordNumList)
     i = i + 1
-    if i > 5000:
+    if i > 50000:
         break
 embedFile.close()
 
@@ -87,24 +110,40 @@ for word, i in wordIndex.items():
     if embedVector is not None:
        embedMatrix[i] = embedVector
 
-#Create The Model
+#Check The Embedding Dimensions
+print("EMBEDING DIM: ", embedDim)
+print("\n")
+
+#Create The Model, A Single Embedding Layer, 1/2 LSTM Layers, A Flatten Layer (For Multiple Classification)
+#And A Dense Layer With 1,2, or 5 Nodes Depending On Number Of Classification Categories
 print("Creating The Model...\n")
 model = Sequential()
-model.add(Embedding(len(wordIndex)+1, embedDim, input_length=100, weights=[embedMatrix], trainable=True)) 
-model.add(LSTM(125, dropout=0.4, recurrent_dropout=0.4, return_sequences=True))
-model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Embedding(len(wordIndex)+1, embedDim, input_length=75, weights=[embedMatrix], trainable=True)) 
+#model.add(LSTM(75, dropout=0.2, recurrent_dropout=0.3, return_sequences=True))
+model.add(LSTM(100, dropout=0.3, recurrent_dropout=0.2, return_sequences=True))
+
+#5-Sentiment
+model.add(Flatten())
+model.add(Dense(5, activation='softmax'))
+#model.add(Dense(2, activation='softmax'))
+#model.add(Dense(1, activation='sigmoid'))
+
+#Print Model Summary
 model.summary()
 
 
-#Compiling The Model With Loss, Optimizer Functions
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+#Compiling The Model
+#Binary-Sentiment
+#model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-#Training And Testing Model
+#5-Sentiment
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+#Train And Test The Model
 print("Train And Test The Model...\n")
-history = model.fit(x_train, y_train, batch_size=50, epochs=2, validation_data=(x_test, y_test)) 
+history = model.fit(x_train, y_train, batch_size=75, epochs=10, validation_data=(x_test, y_test)) 
 
-#Plot The Results
+#Use Pyplot to Plot The Results
 pyplot.plot(history.history['loss'])
 pyplot.plot(history.history['val_loss'])
 pyplot.plot(history.history['acc'])
